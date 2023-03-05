804b744f8ee1947d9343ae9f79d06314086dd092
Mavis Liang <liangxw27@gmail.comgit config --global user.email liangxw27@gmail.com>
2023-03-05 00:05:09 -0500

    Dealed with Andrew's comments

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -1,50 +1,3 @@
-
-####################### Helper functions ######################
-
-########## For cholesky ########
-
-chol_finder <- function(design, outcome){
-  XTX <- t(design) %*% design
-  mle_coef <- solve(XTX, t(design) %*% outcome)
-  return(as.vector(mle_coef))
-}
-
-
-############ FOR BFGS ############
-## The log-likelihood function
-gaussian_logLi <- function(beta, X, Y, noise_var = 1){
-
-  f <- -0.5 * sum((Y - X %*% beta)^2) / noise_var
-  return(f)
-}
-## The gradient
-gradient <- function(beta, X = design, Y = outcome){
-
-  gr = as.vector(t(X) %*% (Y - X %*% beta))
-  return(gr)
-}
-
-##
-BFGS_finder <- function(design, outcome, method){
-  init_coef <- rep(0, ncol(design))
-
-  obj_fn <- function(coef) {
-    gaussian_logLi(coef, design, outcome)
-  }
-
-  obj_grad <- function(coef){
-    gradient(coef, design, outcome)
-  }
-
-  BFGS <- stats::optim(init_coef,
-                       obj_fn, obj_grad,
-                       method = method,
-                       control = list(fnscale = -1))
-  return(BFGS$par)
-}
-
-
-##################### Exported functions ####################
 #' @export hiper_glm
 hiper_glm <- function(design, outcome, model='linear', option = list()){
 
@@ -54,11 +7,10 @@ hiper_glm <- function(design, outcome, model='linear', option = list()){
                                                    model))
   }
 
-  warning("The function is continuing to be implemented")
 
   if (is.null(option$mle_solver)) {
     if (model == 'linear') {
-      beta_est <- chol_finder(design, outcome)
+      beta_est <- pseudoinverse_finder(design, outcome)
     } else {
       # TODO: implement iteratively reweighted least-sq
       stop("Not yet implemented.")
@@ -70,7 +22,7 @@ hiper_glm <- function(design, outcome, model='linear', option = list()){
   ## Output
   hglm_out <- list(coef = beta_est)
   class(hglm_out) <- "hglm"
-  hglm_out
+  return(hglm_out)
 }
 
 

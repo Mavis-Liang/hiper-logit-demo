9d108f513dd0da161250d1e87a7ef0f08a0da289
Mavis Liang <liangxw27@gmail.comgit config --global user.email liangxw27@gmail.com>
2023-03-05 23:37:33 -0500

    Roughly completed. bugs in newton's method unfixed. test still fails

--- tests/testthat/test_grad.R
+++ tests/testthat/test_grad.R
@@ -15,7 +15,7 @@ test_that("linear model's analytical gradient is close to numerical one", {
   n_obs <- 32; n_pred <- 4
   data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
   loglik_func <- function (coef) {
-    gaussian_logLi(coef, data$design, data$outcome)
+    gaussian_loglik(coef, data$design, data$outcome)
   }
   set.seed(615)
   n_test <- 10
@@ -23,7 +23,7 @@ test_that("linear model's analytical gradient is close to numerical one", {
   for (i in 1:n_test) {
     if (!grads_are_close) break
     regcoef <- rnorm(n_pred)
-    analytical_grad <- gradient(regcoef, data$design, data$outcome)
+    analytical_grad <- linear_gradient(regcoef, data$design, data$outcome)
     numerical_grad <- approx_grad(loglik_func, regcoef)
     grads_are_close <- are_all_close(
       analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
@@ -31,3 +31,38 @@ test_that("linear model's analytical gradient is close to numerical one", {
   }
   expect_true(grads_are_close)
 })
+
+
+test_that("logistic model's analytical gradient is close to numerical one", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+
+  loglik_func <- function (coef) {
+    logit_loglik(coef, data$design, data$outcome)
+  }
+
+  set.seed(615)
+  n_test <- 10
+  grads_are_close <- TRUE
+  for (i in 1:n_test) {
+    if (!grads_are_close) break
+    regcoef <- rnorm(n_pred)
+    analytical_grad <- logit_gradient(regcoef, data$design, data$outcome)
+    numerical_grad <- approx_grad(loglik_func, regcoef)
+    grads_are_close <- are_all_close(
+      analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
+    )
+  }
+  expect_true(grads_are_close)
+})
+
+
+
+
+
+
+
+
+
+
